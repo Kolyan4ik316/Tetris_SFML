a88e59de142618a0d1f2810fe0cacff81275b438
Niki Nick <Kolyanius@i.ua>
2022-01-06 23:58:30 +0300

    A window!

--- Tetris_SFML/Tetris_SFML/main.cpp
+++ Tetris_SFML/Tetris_SFML/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World!";
+	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(400, 400), "Window");
+	sf::RectangleShape rect(sf::Vector2f(300.0f, 100.0f));
+	rect.setFillColor(sf::Color::Cyan);
+	while (window->isOpen())
+	{
+		sf::Event m_event;
+
+		while (window->pollEvent(m_event))
+		{
+			if (m_event.type == sf::Event::Closed)
+			{
+				window->close();
+			}
+		}
+		window->clear();
+		window->draw(rect);
+		window->display();
+
+	}
+	delete window;
+	window = NULL;
 }
